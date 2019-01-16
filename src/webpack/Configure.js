cbf29dbed66223f8ace41ca286b1b46a4430a3ce
sunkeysun <sunkeysun@tencent.com>
2019-01-16 12:03:33 +0800

    构建入口到系统临时目录

--- src/webpack/Configure.js
+++ src/webpack/Configure.js
@@ -64,7 +64,7 @@ export default class WebpackConfigure {
     }
 
     get _entryDir() {
-        return path.resolve(__dirname, 'tmp')
+        return path.resolve(os.tmpDir(), '/inventor-dev')
     }
 
     get _moduleConfig() {
